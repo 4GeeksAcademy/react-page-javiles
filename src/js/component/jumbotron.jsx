3f73f1ceb4569e3013c21c22b8f42d3f56045486
Juan Carlos <jeffycr@gmail.com>
2024-10-11 21:26:49 +0000

    Landing Page React

---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export const Jumbo = () => {
+	return (
+		<div className=" container mt-2  p-5 bg-light">
+			<h1>Jumbotron Example</h1>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure at animi, delectus doloribus reprehenderit nisi molestias voluptatum aut provident harum quam dolore nostrum quaerat possimus officiis amet, esse dolores unde?</p>
+			<button type="button" className="btn btn-secondary">Secondary</button>
+		</div>
+	);
+};
+
